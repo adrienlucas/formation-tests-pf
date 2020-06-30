a0e6375d4e1d9e6f6b95f0a94157c552991d773c
Adrien LUCAS <adrien.lucas@smile.fr>
2020-06-30 11:45:59 +0200

    Show categories on homepage

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +13,12 @@ class HomepageController extends AbstractController
      */
     public function index()
     {
-        return $this->render('homepage/index.html.twig');
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        return $this->render('homepage/index.html.twig', [
+            'categories' => $categories
+        ]);
     }
 }
--- tests/HomepageControllerTest.php
+++ tests/HomepageControllerTest.php
@@ -12,7 +12,6 @@ class HomepageControllerTest extends PantherTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-//        var_dump($crawler->filter('h1')->text());
         $this->assertSelectorTextContains('h1', 'Bonjour tout le monde !');
 
         $this->assertCount(2, $crawler->filter('ul.categories li'));
