c565aa14b81de22bc0088544705b302fb9022b2a
Adrien LUCAS <adrien.lucas@smile.fr>
2020-06-30 10:48:46 +0200

    Add HomepageController

---
+++ tests/HomepageControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class HomepageControllerTest extends PantherTestCase
+{
+    public function testTheHomepageIsWellDisplayed()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+//        var_dump($crawler->filter('h1')->text());
+        $this->assertSelectorTextContains('h1', 'Bonjour tout le monde !');
+    }
+}
